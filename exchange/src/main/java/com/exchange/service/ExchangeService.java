6b14075f8567a26cb732c7e8a34bede0118ded68
Sergey <116033520+mynameisSergey@users.noreply.github.com>
2025-10-25 22:35:27 +0300

    feat: add metrics ExchangeService

--- exchange/src/main/java/com/exchange/service/ExchangeService.java
+++ exchange/src/main/java/com/exchange/service/ExchangeService.java
@@ -1,21 +1,26 @@
 package com.exchange.service;
 
+import com.exchange.metrics.CustomMetrics;
 import com.exchange.model.dto.CurrencyEnum;
 import com.exchange.model.dto.ExchangeDto;
 import com.exchange.model.entities.Exchange;
 import com.exchange.repository.ExchangeRepository;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 
 import java.util.Objects;
 
 @Service
 @RequiredArgsConstructor
+@Slf4j
 public class ExchangeService {
 
     private final ExchangeRepository exchangeRepository;
+    private final CustomMetrics customMetrics;
 
     public void setExchange(ExchangeDto exchangeDto) {
+        log.info("сохранение курса валюты {}: {}", exchangeDto.getCurrency().name(), exchangeDto.getValue());
         Exchange exchange = exchangeRepository.findByCurrency(exchangeDto.getCurrency());
         if(Objects.isNull(exchange)) {
             exchange = new Exchange();
@@ -23,6 +28,7 @@ public class ExchangeService {
         }
         exchange.setValue(exchangeDto.getValue());
         exchangeRepository.save(exchange);
+        customMetrics.incrementCurrencyUpdate();
     }
 
     public Double getExchange(CurrencyEnum currency) {
