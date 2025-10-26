544ca9a66807b10420c1380344119418389696c8
Sergey <116033520+mynameisSergey@users.noreply.github.com>
2025-10-26 14:01:10 +0300

    feat: add metrics NotificationsService

--- notifications/src/main/java/com/notifications/service/NotificationsService.java
+++ notifications/src/main/java/com/notifications/service/NotificationsService.java
@@ -1,13 +1,30 @@
 package com.notifications.service;
 
 import com.notifications.dto.NotificationDto;
+import com.notifications.metrics.CustomMetrics;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 
+@Slf4j
 @Service
 public class NotificationsService {
 
+    private Integer notificationCount = 0;
+
+    private final CustomMetrics customMetrics;
+
+    public NotificationsService(CustomMetrics customMetrics) {
+        this.customMetrics = customMetrics;
+    }
+
     public void sendNotification(NotificationDto notificationDto) {
-        System.out.printf("Уведомление для пользователя %s: %s%n",
+        notificationCount++;
+        if (notificationCount % 2 == 0) {
+            customMetrics.incrementFailureNotifications(notificationDto.getLogin());
+            return;
+        }
+        log.info("Уведомление для пользователя {}: {}",
                 notificationDto.getLogin(), notificationDto.getMessage());
     }
 }
